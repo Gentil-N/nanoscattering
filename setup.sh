82a407d2157703c819943e361b1e7bbc1cec77e7
Gentil-N <ngentil77@gmail.com>
2022-06-24 12:57:14 +0200

    setup added

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "------- Setting Up Environment... -------"
+virtualenv venv
+source ./venv/bin/activate
+pip install numpy matplotlib scipy plyfile pycollada
+deactivate
+echo "------- Installation Successful! -------"
